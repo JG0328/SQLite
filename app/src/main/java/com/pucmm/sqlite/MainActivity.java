d61cdd6b1be50d61c83574a306017087282c6114
José Gabriel González <jgg032899@gmail.com>
2020-11-24 23:36:15 -0400

    Working on views

--- app/src/main/java/com/pucmm/sqlite/MainActivity.java
+++ app/src/main/java/com/pucmm/sqlite/MainActivity.java
@@ -1,9 +1,11 @@
 package com.pucmm.sqlite;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +17,26 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.my_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.add_product) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, new ProductsFragment(), "products").addToBackStack("products").commit();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+
+        if(count == 0){
+            super.onBackPressed();
+        }else{
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment, new ProductsList()).commit();
+        }
     }
 }
